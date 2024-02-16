0a97dd4abcf2d9c4e3cdf330b994edb5447929f8
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-02-16 09:01:42 +0530

    added action creators

--- src/store.js
+++ src/store.js
@@ -24,14 +24,38 @@ function reducer(state = initialState, action){
 
 const store = createStore(reducer)
 
-store.dispatch({type: 'account/deposit', payload: 500});
-console.log(store.getState());
+// store.dispatch({type: 'account/deposit', payload: 500});
+// console.log(store.getState());
 
-store.dispatch({type: 'account/withdraw', payload: 300});
-console.log(store.getState());
+// store.dispatch({type: 'account/withdraw', payload: 300});
+// console.log(store.getState());
 
-store.dispatch({type: 'account/requestLoan', payload: {loan: 1000, loanPurpose: 'buy a car'}})
-console.log(store.getState());
+// store.dispatch({type: 'account/requestLoan', payload: {loan: 1000, loanPurpose: 'buy a car'}})
+// console.log(store.getState());
 
-store.dispatch({type: 'account/payLoan'})
+// store.dispatch({type: 'account/payLoan'})
+// console.log(store.getState());
+
+
+// Action creator functions
+function deposit(amount){
+    return {type: "account/deposit", payload: amount}
+}
+function withdraw(amount){
+    return {type: "account/withdraw", payload: amount}
+}
+function requestLoan(amount, purpose){
+    return {type: 'account/requestLoan', payload: {loan: amount, loanPurpose: purpose}}
+}
+function payLoan(){
+    return {type: 'account/payLoan'}
+}
+
+store.dispatch(deposit(500))
+console.log(store.getState());
+store.dispatch(withdraw(200))
+console.log(store.getState());
+store.dispatch(requestLoan(1000, 'buy a car'))
+console.log(store.getState());
+store.dispatch(payLoan())
 console.log(store.getState());
