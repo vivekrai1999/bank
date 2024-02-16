6c6a760ab1e9bcce32b197612a88d51cf311ebef
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-02-16 08:45:57 +0530

    Added store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
     balance: 0,
     loan: 0,
@@ -10,9 +12,26 @@ function reducer(state = initialState, action){
             return {...state, balance: state.balance + action.payload}
         case 'account/withdraw':
             return {...state, balance: state.balance - action.payload}
+        case 'account/requestLoan':
+            if(state.loan>0) return state;
+            return {...state, loan: action.payload.loan, loanPurpose: action.payload.loanPurpose, balance: state.balance + action.payload.loan}
         case 'account/payLoan':
             return {...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan}
         default:
             return state
     }
 }
+
+const store = createStore(reducer)
+
+store.dispatch({type: 'account/deposit', payload: 500});
+console.log(store.getState());
+
+store.dispatch({type: 'account/withdraw', payload: 300});
+console.log(store.getState());
+
+store.dispatch({type: 'account/requestLoan', payload: {loan: 1000, loanPurpose: 'buy a car'}})
+console.log(store.getState());
+
+store.dispatch({type: 'account/payLoan'})
+console.log(store.getState());
