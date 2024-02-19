c500beddc0965ab79cf1e653021eed4eea1b6f64
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-02-19 09:15:45 +0530

    used redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import reducerAccount from "./features/account/accountSlice";
 import reducerCustomer from "./features/customer/customerSlice";
-import { thunk } from "redux-thunk";
 
-const rootReducer = combineReducers({
-    account: reducerAccount,
-    customer: reducerCustomer,
+
+const store = configureStore({
+    reducer: {
+        account: reducerAccount,
+        customer: reducerCustomer,
+    }
 })
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export default store
